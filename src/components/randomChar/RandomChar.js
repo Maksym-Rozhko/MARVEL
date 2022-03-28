0f27fd554c83f7a9a95dd8715190818a369075a1
Maksym <maksymRozhko7@gmail.com>
2022-03-29 01:23:57 +0300

    feat: spinner and error message, refactor: load random character

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,4 +1,6 @@
 import { Component } from 'react/cjs/react.production.min';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 
 import './randomChar.scss';
@@ -11,15 +13,27 @@ class RandomChar extends Component {
     }
 
     state = {
-        char: {}
+        char: {},
+        loading: true,
     };
 
     marvelService = new MarvelService();
 
 
     onCharLoaded = (char) => {
-        this.setState({ char });
+        this.setState({ 
+            char,
+            loading : false,
+            error: false,
+        });
     }
+
+    onError = () => {
+        this.setState({ 
+            loading : false,
+            error: true,
+        });
+    };
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
@@ -27,36 +41,20 @@ class RandomChar extends Component {
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
+            .catch(this.onError);
     }
 
-    charDesciptionLength = (descr) => {
-        if (descr) {
-            return descr.length < 210 ? descr : `${descr.slice(0, 210)}...`;
-        }  
-    };
-
     render() {
-        const { char: { name, description, thumbnail, homepage, wiki }} = this.state;
+        const { char, loading, error } = this.state;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(error || loading) ? <View char={char} /> : null;
 
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {this.charDesciptionLength(description) || `${name} - this character doesn't have a brief description, follow the links below for more information.`}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -74,5 +72,35 @@ class RandomChar extends Component {
         )
     }
 }
+
+const View = ({ char }) => {
+    const { name, description, thumbnail, homepage, wiki } = char;
+
+    const charDesciptionLength = (descr) => {
+        if (descr) {
+            return descr.length < 210 ? descr : `${descr.slice(0, 210)}...`;
+        }  
+    };
+
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {charDesciptionLength(description) || `${name} - this character doesn't have a brief description, follow the links below for more information.`}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+};
 
 export default RandomChar;
